30910100572a06372e6b8dbd1480f40a2a37ce8c
shibasparta <sdeb@spartaglobal.com>
2023-05-04 13:23:07 +0100

    project cards updated - buttons now placed bottom of card

--- src/Projects.js
+++ src/Projects.js
@@ -10,22 +10,25 @@ import * as FontAwesome from 'react-icons/fa'
 function Projects(props) {
     return (
         <section id='projects_section'>
-            <Container>
+            <Container className='container'>
                 <h2 className='text-center'>Projects</h2>
                 <Row>                  
                     {props.allProjects.map((project) => (
                         <Col sm={6} md={4} className='card-group'>
-                            <Card className='my_card'>
+                            <Card className='my_card d-flex align-items-start flex-column'>
                                 <Card.Body>
                                     <img src={project.image}></img>
                                     <Card.Title>{project.name}</Card.Title>
                                     <Card.Text>{project.description}</Card.Text>
                                 </Card.Body>
-                                <Card.Footer>
-                                    <Button className='my_button' href={project.url} target="_blank">Visit</Button>
-                                    <Button className='my_button' href={project.github} target="_blank">
-                                        {React.createElement(FontAwesome['FaGithubSquare'])}
-                                    </Button>
+                                <Card.Footer className='w-100 align-text-bottom d-flex align-items-end flex-column'>
+                                    <div className=''>
+                                        <Button className='my_button' href={project.url} target="_blank">Visit</Button>
+                                        <Button className='my_button' href={project.github} target="_blank">
+                                            {React.createElement(FontAwesome['FaGithubSquare'])}
+                                        </Button>
+                                    </div>
+                                    
                                 </Card.Footer>
                             </Card>
                         </Col>
