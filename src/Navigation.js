e9202854651728580a2e1fc5440fe520f91efcbd
shibasparta <sdeb@spartaglobal.com>
2023-05-06 12:35:04 +0100

    navigation uddated

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,15 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {AiFillHome, AiFillAppstore, AiFillContacts} from 'react-icons/ai'
-import {MdNaturePeople} from 'react-icons/md'
 import Nav from 'react-bootstrap/Nav'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faFaceSmile, faCode, faHand } from '@fortawesome/free-solid-svg-icons'
 
 function Navigation() {
     return (
         <Nav className='my_navigation'>
-            <a href="#"><AiFillHome/></a>
-            <a href="#aboutme_section"><MdNaturePeople/></a>
-            <a href="#projects_section"><AiFillAppstore/></a>
-            <a href="#contact_section"><AiFillContacts/></a>
+            {/* <a href="#"><AiFillHome/></a> */}
+            <a href="#"><FontAwesomeIcon icon={faHouse} /></a>
+            <a href="#aboutme_section"><FontAwesomeIcon icon={faFaceSmile} /></a>
+            <a href="#projects_section"><FontAwesomeIcon icon={faCode} /></a>
+            <a href="#contact_section"><FontAwesomeIcon icon={faHand} /></a>
         </Nav>
     )
 }
